beab21d3f0a9254a2ced0758370f207406a4d555
Vishal Gavade <120453822+GavadeVishal@users.noreply.github.com>
2024-06-17 10:51:36 +0530

    Uploaded on 17-06-2024

---
+++ Inheritance_Example/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SimpleInheritance
+{
+    // Base class
+    public class Car
+    {
+        public string Brand { get; set; }
+
+        public Car(string brand)
+        {
+            Brand = brand;
+        }
+
+        public void Drive()
+        {
+            Console.WriteLine($"{Brand} is driving.");
+        }
+    }
+
+    // Derived class
+    public class SportsCar : Car
+    {
+        public SportsCar(string brand) : base(brand) { }
+
+        public void TurboBoost()
+        {
+            Console.WriteLine($"{Brand} is using turbo boost!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car("Toyota");
+            myCar.Drive();
+
+            SportsCar mySportsCar = new SportsCar("Ferrari");
+            mySportsCar.Drive();
+            mySportsCar.TurboBoost();
+        }
+    }
+}
